1312af10232571d5881a9b383ddd70f743d9abeb
Mugabe Ignace <ignacemugabe@gmail.com>
2023-05-06 16:13:51 +0200

    pass: 02_repeatString exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = (val, period) =>
+{
+    let word = '';
+    if(period < 0){
+        return 'ERROR';
+    }
+    for (let i = 0; i < period; i++)
+    {
+        word += val;
+    }
+    return word;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
